a9a0bac2b351ee8532e8bc4b47dbfdba0f3461af
Bri Stanback <stanback@users.noreply.github.com>
2026-02-05 22:35:32 -0800

    add: twitter:site and twitter:creator @Stanback for card attribution

--- src/render.ts
+++ src/render.ts
@@ -174,6 +174,8 @@ function baseTemplate(options: {
 
   <!-- Twitter -->
   <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@Stanback">
+  <meta name="twitter:creator" content="@Stanback">
   <meta name="twitter:title" content="${title}">
   <meta name="twitter:description" content="${description}">
   <meta name="twitter:image" content="${ogImage}">
